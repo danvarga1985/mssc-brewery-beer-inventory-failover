9a4f56619284ce333a45dd4f8ff4f73128d46411
danvarga1985 <danvarga1985@gmail.com>
2021-02-02 16:43:14 +0100

    Switching to sfg-brewery BOM, Adding Dockerfile

--- src/main/java/com/danvarga/msscbeerinventoryfailover/web/InventoryHandler.java
+++ src/main/java/com/danvarga/msscbeerinventoryfailover/web/InventoryHandler.java
@@ -1,29 +1,59 @@
 package com.danvarga.msscbeerinventoryfailover.web;
 
+// MVC impl - for using local BOM
+
+//import com.danvarga.brewery.model.BeerInventoryDto;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.function.HandlerFunction;
+//import org.springframework.web.servlet.function.ServerRequest;
+//import org.springframework.web.servlet.function.ServerResponse;
+//
+//import java.time.OffsetDateTime;
+//import java.util.Collections;
+//import java.util.UUID;
+//
+//@Component
+//public class InventoryHandler implements HandlerFunction {
+//
+//    @Override
+//    public ServerResponse handle(ServerRequest request) throws Exception {
+//        return ServerResponse.ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(Collections.singletonList(BeerInventoryDto.builder()
+//                        .id(UUID.randomUUID())
+//                        .beerId(new UUID(0, 0))
+//                        .quantityOnHand(999)
+//                        .createdDate(OffsetDateTime.now())
+//                        .lastModifiedDate(OffsetDateTime.now())
+//                        .build()));
+//    }
+//}
+
 import com.danvarga.brewery.model.BeerInventoryDto;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.HandlerFunction;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Component
-public class InventoryHandler implements HandlerFunction {
-
-    @Override
-    public ServerResponse handle(ServerRequest request) throws Exception {
+public class InventoryHandler {
+    public Mono<ServerResponse> listInventory(ServerRequest request){
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Collections.singletonList(BeerInventoryDto.builder()
-                        .id(UUID.randomUUID())
-                        .beerId(new UUID(0, 0))
-                        .quantityOnHand(999)
-                        .createdDate(OffsetDateTime.now())
-                        .lastModifiedDate(OffsetDateTime.now())
-                        .build()));
+                .contentType(MediaType.APPLICATION_STREAM_JSON)
+                .body(Mono.just(Arrays.asList(
+                        BeerInventoryDto.builder()
+                                .id(UUID.randomUUID())
+                                .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                                .quantityOnHand(999)
+                                .createdDate(OffsetDateTime.now())
+                                .lastModifiedDate(OffsetDateTime.now())
+                                .build())), List.class);
     }
 }
--- src/main/java/com/danvarga/msscbeerinventoryfailover/web/RouterConfig.java
+++ src/main/java/com/danvarga/msscbeerinventoryfailover/web/RouterConfig.java
@@ -1,20 +1,37 @@
 package com.danvarga.msscbeerinventoryfailover.web;
 
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.servlet.function.RouterFunction;
+//
+//import static org.springframework.web.servlet.function.RequestPredicates.GET;
+//import static org.springframework.web.servlet.function.RequestPredicates.accept;
+//import static org.springframework.web.servlet.function.RouterFunctions.route;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
 
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
-import static org.springframework.web.servlet.function.RequestPredicates.accept;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Configuration
 public class RouterConfig {
 
+    // MVC impl - for local BOM
+//    @Bean
+//    public RouterFunction inventoryRoute(InventoryHandler inventoryHandler) {
+//        return route(GET("/inventory-failover").and(accept(MediaType.APPLICATION_JSON)),
+//                inventoryHandler);
+//    }
+
+
     @Bean
     public RouterFunction inventoryRoute(InventoryHandler inventoryHandler) {
         return route(GET("/inventory-failover").and(accept(MediaType.APPLICATION_JSON)),
-                inventoryHandler);
+                inventoryHandler::listInventory);
     }
 }
